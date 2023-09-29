cbb9d21876f46b2a5255f3290b6ea867985159b9
braian villaverde <braianvillaverde@gmail.com>
2023-09-29 17:52:57 -0300

    Primer commit

---
+++ Js/Jscript.js
@@ -0,0 +1,78 @@
+let baseDatos;
+// fetch ("../Json/productos.json").
+// then((response)=>response.json())
+// .then((json)=>{
+//      localStorage.setItem("productos",JSON.stringify(json));
+// });
+
+// const productos=localStorage.getItem("productos")
+// const baseDatos=JSON.parse(productos);
+
+const domItems=document.querySelector("#items");
+
+const productoSeleccionado=[];
+
+
+const cargarProductos=async()=>{ 
+    baseDatos=localStorage.getItem("productos");
+    if (baseDatos == null){
+        const response = await fetch("Json/productos.json");
+        productos = await response.json();
+
+        localStorage.setItem("productos", JSON.stringify(productos));
+    }
+    
+    if(typeof baseDatos=="string"){
+        baseDatos=JSON.parse(baseDatos);
+    }
+    
+    baseDatos.forEach(producto => {
+        const card=document.createElement("div");
+        card.classList.add("card","col-sm-3");
+        const imagen=document.createElement("img");
+        imagen.classList.add("card-img-top","img-thumbnail");
+        imagen.setAttribute("src", producto.imagen);
+        const contenido=document.createElement("div");
+        contenido.classList.add("card-body");
+        const title=document.createElement("h5");
+        title.classList.add("card-title");
+        title.textContent=producto.nombre;
+        const desc=document.createElement("p");
+        desc.classList.add("card-text");
+        desc.textContent=producto.descripcion;
+        const enlace=document.createElement("a");
+        enlace.classList.add("card-link");
+        enlace.setAttribute("href","../detalles.html");
+        enlace.textContent="...Mas detalles";
+
+        contenido.appendChild(title);
+        contenido.appendChild(desc);
+        contenido.appendChild(enlace);
+        card.appendChild(imagen);
+        card.appendChild(contenido);
+        domItems.appendChild(card);
+
+        
+
+        
+        enlace.addEventListener("click",()=>{
+            productoSeleccionado.push({
+                nombre:producto.nombre,
+                descripcion:producto.descripcion,
+                imagen:producto.imagen,
+                detalle:producto.detalle,
+                precio:producto.precio,
+                nivel:producto.nivel,
+            })
+            localStorage.setItem("producto", JSON.stringify(productoSeleccionado));
+            console.log(productoSeleccionado);
+            location.href="detalles.html";
+        });
+
+
+    });
+
+};
+
+cargarProductos();
+
---
+++ Js/Jsproducto.js
@@ -0,0 +1,49 @@
+const detalle=JSON.parse(localStorage.getItem("producto"));
+
+const domItems2=document.querySelector("#items2");
+const btn=document.querySelector("#volver");
+
+  detalle.forEach(producto=>{
+    // const longEstrellas=producto.nivel.length;
+    // const iconEstrellas = ["☠", "☠", "☠", "☠", "☠"];
+     const card=document.createElement("div");
+     card.classList.add("card","col-sm-3","d-flex","justify-content-center");
+      const imagen=document.createElement("img");
+      imagen.classList.add("card-img-top");
+      imagen.setAttribute("src", producto.imagen);
+      const contenido=document.createElement("div");
+      contenido.classList.add("card-body");
+      const title=document.createElement("h2");
+      title.classList.add("card-title");
+      title.textContent=producto.nombre;
+      const desc=document.createElement("p");
+      desc.classList.add("card-text");
+      desc.textContent=producto.descripcion;
+      const det=document.createElement("p");
+      det.classList.add("card-text");
+      det.textContent=producto.detalle;
+      const valor=document.createElement("p");
+      valor.classList.add("card-text");
+      valor.textContent=producto.precio;
+      const estrellas = ["😭", "😭", "😭", "😭", "😭"];
+      const nivel=document.createElement("p");
+      nivel.classList.add("card-text");
+      for (let i = 0; i < producto.nivel.length; i++) {
+        estrellas[i] = "😃";
+      }
+      nivel.textContent = estrellas.join("");
+
+      contenido.appendChild(title);
+      contenido.appendChild(desc);
+      contenido.appendChild(det);
+      contenido.appendChild(valor);
+      contenido.appendChild(nivel);
+      card.appendChild(imagen);
+      card.appendChild(contenido);
+      domItems2.appendChild(card);
+    });
+    
+
+btn.addEventListener("click",()=>{
+  localStorage.removeItem("producto");
+})
